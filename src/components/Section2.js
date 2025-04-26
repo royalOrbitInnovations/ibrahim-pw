7c08fb8e3e441cd66113521befc4afc9740c2b32
Melvin <melvinmps11301@gmail.com>
2025-04-26 11:36:41 +0300

    updated section 2

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -6,11 +6,10 @@ import { courses } from "@/data/courses";
 export default function Section2() {
   return (
     <div className="pt-[10rem] min-h-[100vh]">
-      <ScrollingText text="courses" baseVelocity={200} size={3} />
+      <ScrollingText text="courses & workshops" baseVelocity={200} size={3} />
       <div className="px-[15rem]">
         <Carousel data={courses} linkText="Get Details" />
       </div>
-      <ScrollingText text="workshops" baseVelocity={200} size={3} />
       <div className="px-[15rem]">
         <Carousel data={workshops} linkText="Explore Workshop" />
       </div>
